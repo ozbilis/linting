b4b528295036fc5327f88939823c61fa7b7e6e35
Burak Ozbilis <burak@oodit.nl>
2023-10-13 11:35:44 +0200

    good file

--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
+const a = 200;
 
-
-var a=200
-
-function add(n1, n2) {
-    return n1+n2
+function add(n1, n2): number {
+  return n1 + n2;
 }
 
+add(a, 1);
 
-
-
-add(a,1)
-
-
-
-function isEqual(n1, n2) {
-    return n1==n2
+function isEqual(n1, n2): boolean {
+  return n1 === n2;
 }
 
-
-//var b = isEqual(1,1);
-
+isEqual(1, 1);
